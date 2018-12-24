057306037ff5b4d1a98436fce5f280feb2271c7a
Katie <katie_acting@live.ca>
2018-12-23 20:02:52 -0500

    initial commit

---
+++ receipts.py
@@ -0,0 +1,27 @@
+from glob2 import glob
+import textract
+
+
+docs = glob("./receipts/*")
+full_text = []
+sum = 0
+
+def init():
+    for doc in docs:
+        if doc != "./receipts\desktop.ini":
+            try:
+                text = textract.process(doc)
+                half_text = text.split("Total Amount: ")[1]
+                total = str(half_text.split('\n')[0])
+                total_num = total.replace("$", "")
+                global sum
+                sum += int(total_num)
+            except:
+                print "error at " + doc
+        else:
+            pass
+        
+
+    print sum
+
+init()
