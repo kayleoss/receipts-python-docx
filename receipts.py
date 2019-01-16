c4cba37d5a8a300e08c42740ac4824adfaa0819d
kayleoss <katie_acting@live.ca>
2019-01-15 20:25:10 -0500

    add separation between rmt and acu totals

--- receipts.py
+++ receipts.py
@@ -5,6 +5,8 @@ import textract
 docs = glob("./receipts/*")
 full_text = []
 sum = 0
+acu_sum = 0
+rmt_sum = 0
 
 def init():
     for doc in docs:
@@ -12,16 +14,31 @@ def init():
             try:
                 text = textract.process(doc)
                 half_text = text.split("Total Amount: ")[1]
+                service_type_half = text.split("Description Of Service: ")[1]
+                service_type = str(service_type_half.split('\n')[0])
                 total = str(half_text.split('\n')[0])
                 total_num = total.replace("$", "")
                 global sum
                 sum += int(total_num)
+
+                if (service_type.lower() == "acupuncture treatment") or (service_type.lower() == "acupuncrure treatment"):
+                    global acu_sum
+                    acu_sum += int(total_num)
+                elif service_type.lower() == "massage therapy":
+                    global rmt_sum
+                    rmt_sum += int(total_num)
+                else:
+                    print service_type.lower()
+
             except:
                 print "error at " + doc
         else:
             pass
         
 
-    print sum
+    print "TOTAL: " + str(sum)
+    print "ACUPUNCTURE TOTAL: " + str(acu_sum)
+    print "RMT TOTAL: " + str(rmt_sum)
+
 
 init()
